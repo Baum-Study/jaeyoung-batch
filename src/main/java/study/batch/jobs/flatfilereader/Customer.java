28e834b2b44d0b5d140f0fd7338c332e4ea00291
bangjaeyoung <qkdwo4613@gmail.com>
2024-12-01 20:16:43 +0900

    create Customer Job - FlatFileItemReader, FlatFileItemWriter

---
+++ src/main/java/study/batch/jobs/flatfilereader/Customer.java
@@ -0,0 +1,13 @@
+package study.batch.jobs.flatfilereader;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+    
+    private String name;
+    private int age;
+    private String gender;
+}
---
+++ src/main/java/study/batch/jobs/flatfilereader/CustomerLineAggregator.java
@@ -0,0 +1,11 @@
+package study.batch.jobs.flatfilereader;
+
+import org.springframework.batch.item.file.transform.LineAggregator;
+
+public class CustomerLineAggregator implements LineAggregator<Customer> {
+    
+    @Override
+    public String aggregate(Customer item) {
+        return item.getName() + "," + item.getAge();
+    }
+}
